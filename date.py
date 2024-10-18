b18d266f2d00e85dcfc8f555c4731f5866773246
alexgicimo <gakurualexgicimo@gmail.com>
2024-10-18 14:16:45 +0300

    check i anyone birthday is on todays date and month the append their name to a list

--- date.py
+++ date.py
@@ -4,8 +4,11 @@ class BirthDate:
     def __init__(self):
         self.day =None
         self.month =None
-        self.get_date_today()
+        self.birthday_names = []
         self.birth_dates ={}
+        self.get_date_today()
+        self.get_friends_birthdate()
+        self.is_today_a_birthday()
     def get_date_today(self)->None:
         today = datetime.fromisoformat(str(datetime.now()))
         self.month, self.day = today.month,today.day
@@ -19,6 +22,10 @@ class BirthDate:
                 self.birth_dates = {row.name:[row.month, row.day] for index, row in data.iterrows()}
             except KeyError as error:
                 print(f"{error} ")
+    def is_today_a_birthday(self)->None:
+        for name, birthdate in self.birth_dates.items():
+            if birthdate[0] == self.month and birthdate[1] == self.day:
+                self.birthday_names.append(name)
 
 if __name__ == "__main__":
     birthdate = BirthDate()

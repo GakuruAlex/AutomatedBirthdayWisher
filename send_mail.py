485947bd3b7f5f0dea1e173b5216e8d4bde142ae
alexgicimo <gakurualexgicimo@gmail.com>
2024-10-18 19:06:43 +0300

    doc_str

--- send_mail.py
+++ send_mail.py
@@ -9,13 +9,22 @@ class SendMail:
         self.receiver_email = ""
         load_dotenv(dotenv_path="./data.env")
         self.get_details()
-    def send_mail(self, message: str):
+    def send_mail(self, message: str)->None:
+        """_Create an SMTP object, login with gmail credentials, send mail and close connection_
+
+        Args:
+            message (str): _Chosen Birthday wish_
+        >>>Example
+            send_mail_object.send_mail('Hello John, Happy Birthday')
+        """
         connection = SMTP(host= self.host)
         connection.starttls()
         connection.login(user= self.email, password= self.password)
         connection.sendmail(from_addr= self.email, to_addrs= self.receiver_email , msg= message)
         connection.close()
-    def get_details(self):
+    def get_details(self)->None:
+        """_Read the env file and gets the email credentials_
+        """
         self.email = os.getenv("GMAIL_ACC")
         self.password = os.getenv("GMAIL_APP_PASSWORD")
         self.receiver_email = os.getenv("RECEIVER_EMAIL")

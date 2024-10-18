2eb40392ec6321c00934de960a39b94fe528973f
alexgicimo <gakurualexgicimo@gmail.com>
2024-10-18 14:05:34 +0300

    read the birthdays csv and populate birth_dates dict

--- date.py
+++ date.py
@@ -1,12 +1,24 @@
 from datetime import datetime
-
+from pandas import read_csv
 class BirthDate:
     def __init__(self):
         self.day =None
         self.month =None
-    def get_date_today(self):
+        self.get_date_today()
+        self.birth_dates ={}
+    def get_date_today(self)->None:
         today = datetime.fromisoformat(str(datetime.now()))
         self.month, self.day = today.month,today.day
+    def get_friends_birthdate(self)->None:
+        try:
+            data= read_csv("./birthday.csv")
+        except FileNotFoundError as e:
+            print(f"Error! {e}")
+        finally:
+            try:
+                self.birth_dates = {row.name:[row.month, row.day] for index, row in data.iterrows()}
+            except KeyError as error:
+                print(f"{error} ")
 
 if __name__ == "__main__":
     birthdate = BirthDate()

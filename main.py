e8832c0a26063eea732405516bff5d046e4ba769
alexgicimo <gakurualexgicimo@gmail.com>
2024-10-18 19:00:43 +0300

    main

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from date import BirthDate
+from letter import Letter
+from send_mail import SendMail
 def main():
+    sendmail = SendMail()
+    letter = Letter()
     birthdate = BirthDate()
+    if len(birthdate.birthday_names) > 0:
+        for name in birthdate.birthday_names:
+            letter.replace_name(name)
+            sendmail.send_mail(message=f"Subject:Happy Birthday\n\n{letter.letter}")
+            print(f"Email sent to {name}")
 
 if __name__ == "__main__":
     main()

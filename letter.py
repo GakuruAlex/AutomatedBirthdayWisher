118dc0b9556e20790d36dd432e9975f26ec33e4d
alexgicimo <gakurualexgicimo@gmail.com>
2024-10-18 19:12:25 +0300

    docs_str

--- letter.py
+++ letter.py
@@ -6,6 +6,9 @@ class Letter:
         self.get_letter()
         self.format_letter()
     def get_letter(self)-> None:
+        """_Chose a random int among 1 and 3 to represent the letter to chose , open letter templates
+            and read content of the letter_
+        """
         chosen = randint(1, 3)
         try:
             with open(file=f"./letter_templates/letter_{chosen}.txt") as file:
@@ -13,8 +16,18 @@ class Letter:
         except FileNotFoundError as missing_file:
             print(f"Error {missing_file}")
     def replace_name(self, name: str)-> None:
+        """_Replace the [NAME] placeholder in the letter txt file with a given name_
+
+        Args:
+            name (str): _Name to replace '[NAME]' with_
+        >>>Example
+            letter_object.replace_name('John")
+            output: Hello John, ...
+        """
         self.letter=self.letter.replace("[NAME]", name)
-    def format_letter(self):
+    def format_letter(self)->None:
+        """_Given a list as instance variable, join the str in the list_
+        """
         new_letter = self.letter[:]
         self.letter="".join(new_letter)
 

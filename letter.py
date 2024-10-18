4e9064a07ebdcb215e37b614706c9e65db8e5bfa
alexgicimo <gakurualexgicimo@gmail.com>
2024-10-18 17:04:13 +0300

    replace NAME placeholder with actual friends name

--- letter.py
+++ letter.py
@@ -3,13 +3,15 @@ class Letter:
     def __init__(self):
         self.letter = None
         self.get_letter()
-    def get_letter(self):
+    def get_letter(self)-> None:
         chosen = randint(1, 3)
         try:
             with open(file=f"./letter_templates/letter_{chosen}.txt") as file:
                 self.letter = file.readlines()
         except FileNotFoundError as missing_file:
             print(f"Error {missing_file}")
+    def replace_name(self, name: str)-> None:
+        self.letter[0]=self.letter[0].replace("[NAME]", name)
 
 if __name__ == "__main__":
     letter = Letter()
